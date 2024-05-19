304b980f281078d5cb94d0d50e08a1b156b36c1b
Scottie Enriquez <scottie@enriquez.xxx>
2024-05-18 18:00:35 -0700

    WIP: adding blog posts

---
+++ scottie-is-xxx/src/components/mapMarker.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMapMarker } from "@fortawesome/free-solid-svg-icons"
+
+const MapMarker = () => {
+    return (
+        <div>
+            <FontAwesomeIcon icon={faMapMarker} size="3x" />
+        </div>
+    )
+}
+
+export default MapMarker
---
+++ scottie-is-xxx/src/components/nature-of-code/introduction/randomNormalDistribution.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import BarChart from '../d3/barChart';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRefresh} from "@fortawesome/free-solid-svg-icons";
+
+class RandomNormalDistribution extends Component {
+    generateRandomData() {
+        const datasetSize = 100;
+        const maxValue = 100;
+        const data = [];
+        for(let index = 0; index < datasetSize; index++) {
+            data[index] = {
+                index: index,
+                value: Math.floor(Math.random() * maxValue)
+            }
+        }
+        return data;
+    }
+
+    handleRefreshDataClick = (event) => {
+        this.setState({ data: this.generateRandomData() });
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: this.generateRandomData()
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button style={{ backgroundColor: '#61FFCA', color: 'black' }} onClick={this.handleRefreshDataClick}>
+                    <FontAwesomeIcon icon={faRefresh} /> Generate New Data
+                </button>
+                <BarChart data={this.generateRandomData()} xAxisName={'index'} yAxisName={'value'} />
+            </div>
+        );
+    }
+}
+
+export default RandomNormalDistribution;
