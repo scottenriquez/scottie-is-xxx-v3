b4d6ddaa7cea174550d98992a5d3e7662692278d
Scottie Enriquez <scottie@enriquez.xxx>
2024-05-18 17:45:24 -0700

    WIP: adding skeleton

---
+++ scottie-is-xxx/docusaurus.config.js
@@ -0,0 +1,139 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Scottie Enriquez',
+  tagline: 'I\'m a cloud solutions architect, software engineer, and occasional nomad. I currently live in Los Angeles, California and work for Amazon Web Services. All opinions expressed here are my own.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://scottie.is',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'scottenriquez', // Usually your GitHub org/user name.
+  projectName: 'scottie-is-xxx-v3', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      navbar: {
+        title: 'Scottie Enriquez',
+        logo: {
+          alt: 'Logo',
+          src: 'img/scott-s.svg',
+        },
+        items: [
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/resume', label: 'Resume', position: 'left'},
+          {
+            href: 'https://github.com/scottenriquez',
+            label: 'GitHub',
+            position: 'left',
+          },
+          {
+            href: 'https://www.linkedin.com/in/scottenriquez/',
+            label: 'LinkedIn',
+            position: 'left',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: prismThemes.dracula,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
