a71cd79c6c04a122c836578660390a99dfeaf768
Scottie Enriquez <scottie@enriquez.xxx>
2024-05-25 10:31:41 -0700

    WIP: finished vector introduction

--- scottie-is-xxx/src/components/NatureOfCode/One/NVector/nVector.js
+++ scottie-is-xxx/src/components/NatureOfCode/One/NVector/nVector.js
@@ -13,4 +13,20 @@ class NVector {
       ...this.components.map((component, index) => component + otherVector.components[index])
     );
   }
+
+  // assumes that the second vector has the same dimensions as the first
+  subtract(otherVector) {
+    return new NVector(
+      ...this.components.map((component, index) => component - otherVector.components[index])
+    );
+  }
+
+  scale(scalar) {
+    return new NVector(...this.components.map(component => component * scalar));
+  }
+
+  // assumes that the second vector has the same dimensions as the first
+  dot(otherVector) {
+    return this.components.reduce((sum, component, index) => sum + component * otherVector.components[index], 0);
+  }
 }
